72b2f5e3eae7cdffe02aed282fa81dd571f44a49
Phil-tom <zzz1565789907@163.com>
2020-11-15 09:11:41 +0800

    ProgressBar

---
+++ Chapter3/UIWidgetTest/app/src/main/java/com/zzz/uiwidgettest/MainActivity.java
@@ -0,0 +1,43 @@
+package com.zzz.uiwidgettest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Button button = (Button) findViewById(R.id.button);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+//                if (progressBar.getVisibility() == View.GONE) {
+//                    progressBar.setVisibility(View.VISIBLE);
+//                } else {
+//                    progressBar.setVisibility(View.GONE);
+//                }
+
+                //获取ProgressBar的进度
+                int progress = progressBar.getProgress();
+                progress += 10;
+                //设置ProgressBar的进度
+                progressBar.setProgress(progress);
+                break;
+            default:
+                break;
+        }
+    }
+}
