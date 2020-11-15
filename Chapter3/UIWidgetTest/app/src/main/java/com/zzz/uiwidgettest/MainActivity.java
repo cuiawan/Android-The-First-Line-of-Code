6d872e3a1072b0ae12b9483af793731c3dd42f7d
Phil-tom <zzz1565789907@163.com>
2020-11-15 09:42:51 +0800

    AlertDialog

--- Chapter3/UIWidgetTest/app/src/main/java/com/zzz/uiwidgettest/MainActivity.java
+++ Chapter3/UIWidgetTest/app/src/main/java/com/zzz/uiwidgettest/MainActivity.java
@@ -2,6 +2,8 @@ package com.zzz.uiwidgettest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,17 +26,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.button:
+//                //设置是否可见
 //                if (progressBar.getVisibility() == View.GONE) {
 //                    progressBar.setVisibility(View.VISIBLE);
 //                } else {
 //                    progressBar.setVisibility(View.GONE);
 //                }
 
-                //获取ProgressBar的进度
-                int progress = progressBar.getProgress();
-                progress += 10;
-                //设置ProgressBar的进度
-                progressBar.setProgress(progress);
+//                //设置进度
+//                //获取ProgressBar的进度
+//                int progress = progressBar.getProgress();
+//                progress += 10;
+//                //设置ProgressBar的进度
+//                progressBar.setProgress(progress);
+
+                //AlertDialog对话框置顶于所有界面元素之上，能屏蔽掉其他控件的交互能力
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("This is a dialog")
+                        .setMessage("Something important")
+                        //使用back键关闭该属性
+                        .setCancelable(false)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        }).show();
                 break;
             default:
                 break;
