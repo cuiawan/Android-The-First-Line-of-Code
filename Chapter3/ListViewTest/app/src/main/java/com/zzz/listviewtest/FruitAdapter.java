ba862cc435d633e6ba6ff875d34ef88b4bd6d17a
Phil-tom <zzz1565789907@163.com>
2020-11-18 21:11:49 +0800

    提升ListView的运行效率

--- Chapter3/ListViewTest/app/src/main/java/com/zzz/listviewtest/FruitAdapter.java
+++ Chapter3/ListViewTest/app/src/main/java/com/zzz/listviewtest/FruitAdapter.java
@@ -23,11 +23,26 @@ public class FruitAdapter extends ArrayAdapter<Fruit> {
     @Override
     public View getView(int position, View convertView,  ViewGroup parent) {
         Fruit fruit = getItem(position);//获取当前页面的Fruit实例
-        View view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
-        ImageView fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
-        TextView fruitName = (TextView) view.findViewById(R.id.fruit_name);
-        fruitImage.setImageResource(fruit.getImageId());
-        fruitName.setText(fruit.getName());
+        View view;
+        ViewHolder viewHolder;
+        if(convertView!=null){
+            view = LayoutInflater.from(getContext()).inflate(resourceId,parent,false);
+            viewHolder = new ViewHolder();
+            viewHolder.fruitImage = (ImageView)view.findViewById(R.id.fruit_image);
+            viewHolder.fruitName = (TextView) view.findViewById(R.id.fruit_name);
+            view.setTag(viewHolder);//将viewHolder存储在view中
+
+        }else{
+            view = convertView;
+            viewHolder = (ViewHolder)view.getTag();
+        }
+        viewHolder.fruitImage.setImageResource(fruit.getImageId());
+        viewHolder.fruitName.setText(fruit.getName());
         return view;
+    }
+
+    class ViewHolder{
+        ImageView fruitImage;
+        TextView fruitName;
     }
 }
