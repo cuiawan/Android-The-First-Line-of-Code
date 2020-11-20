36ee08e8920ab7f52f2340f581217941d8dca8c2
Phil-tom <zzz1565789907@163.com>
2020-11-20 18:52:34 +0800

    动态添加碎片

--- Chapter4/FragmentTest/app/src/main/java/com/zzz/fragmenttest/MainActivity.java
+++ Chapter4/FragmentTest/app/src/main/java/com/zzz/fragmenttest/MainActivity.java
@@ -1,8 +1,13 @@
 package com.zzz.fragmenttest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.button:
+                        replaceFragment(new AnotherRightFragment());
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+        replaceFragment(new RightFragment());
     }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.right_layout, fragment);
+        transaction.commit();
+    }
+
 }
