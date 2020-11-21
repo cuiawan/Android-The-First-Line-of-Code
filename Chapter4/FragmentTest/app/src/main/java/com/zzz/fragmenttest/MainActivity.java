1c05d89f204720d43b8aa7f38e5d72154e2a825f
Phil-tom <zzz1565789907@163.com>
2020-11-21 15:44:19 +0800

    动态加载布局

--- Chapter4/FragmentTest/app/src/main/java/com/zzz/fragmenttest/MainActivity.java
+++ Chapter4/FragmentTest/app/src/main/java/com/zzz/fragmenttest/MainActivity.java
@@ -32,11 +32,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void replaceFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.right_layout, fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction transaction = fragmentManager.beginTransaction();
+//        transaction.replace(R.id.right_layout, fragment);
+//        transaction.addToBackStack(null);
+//        transaction.commit();
     }
 
 }
