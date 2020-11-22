68a95c74e19c57a75535d61acbffd2f7d853ba64
Phil-tom <zzz1565789907@163.com>
2020-11-22 19:58:18 +0800

    简化代码

--- Chapter4/FragmentTest/app/src/main/java/com/zzz/fragmenttest/LeftFragment.java
+++ Chapter4/FragmentTest/app/src/main/java/com/zzz/fragmenttest/LeftFragment.java
@@ -12,7 +12,6 @@ public class LeftFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.left_fragment, container, false);
-        return view;
+        return inflater.inflate(R.layout.left_fragment, container, false);
     }
 }
