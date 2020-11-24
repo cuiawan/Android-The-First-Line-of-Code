816c01469955126d1d71c97f9502b95ff1d9bc78
Phil-tom <zzz1565789907@163.com>
2020-11-24 19:59:33 +0800

    本地广播

--- Chapter5/MyBroadcastReceiver/app/src/main/java/com/zzz/mybroadcastreceiver/MainActivity.java
+++ Chapter5/MyBroadcastReceiver/app/src/main/java/com/zzz/mybroadcastreceiver/MainActivity.java
@@ -1,34 +1,64 @@
 package com.zzz.mybroadcastreceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+
+    private IntentFilter intentFilter;
+    private LocalReceiver localReceiver;
+    private LocalBroadcastManager localBroadcastManager;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "onClick: ");
-                Intent intent = new Intent("com.zzz.mybroadcastreceiver.MY_BROADCAST");
-
+                Intent intent = new Intent("com.zzz.mybroadcastreceiver.LOCAL_BROADCAST");
+                localBroadcastManager.sendBroadcast(intent);
                 //在高版本中需要把把其中一行代码加上
 //                intent.setComponent(new ComponentName("com.zzz.mybroadcastreceiver","com.zzz.mybroadcastreceiver.MyBroadcastReceiver"));
 //                intent.setPackage("com.zzz.mybroadcastreceiver2");
-                sendOrderedBroadcast(intent,null);
+                //发送有序广播
+//                sendOrderedBroadcast(intent,null);
+
             }
         });
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("com.zzz.mybroadcastreceiver.LOCAL_BROADCAST");
+        localReceiver = new LocalReceiver();
+        localBroadcastManager.registerReceiver(localReceiver,intentFilter);//注册本地广播监听器
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        localBroadcastManager.unregisterReceiver(localReceiver);
+    }
+
+    class LocalReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context,"received local broadcast",Toast.LENGTH_SHORT).show();
+        }
     }
 }
