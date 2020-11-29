d1387597ff07ab18b34da1b0e0634dcabd626cf3
Phil-tom <zzz1565789907@163.com>
2020-11-29 22:58:09 +0800

    将数据存储到SharedPreferences中

---
+++ Chapter6/SharedPreferencesTest/app/src/main/java/com/zzz/sharedpreferencestest/MainActivity.java
@@ -0,0 +1,29 @@
+package com.zzz.sharedpreferencestest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button saveData = (Button) findViewById(R.id.save_data);
+        saveData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
+                editor.putString("name","Tom");
+                editor.putInt("age",22);
+                editor.putBoolean("married",false);
+                editor.apply();
+            }
+        });
+
+    }
+}
